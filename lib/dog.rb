714359bc68111d456349ee793b0201e896f3814a
Denis Atkesone <denis.atkesone@gmail.com>
2018-02-06 15:33:39 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -25,7 +25,7 @@ class Dog
   end
 
   def self.new_from_db(row)
-    Dogs.new(name: row[1], breed: row[2], id: row[0])
+    Dog.new(name: row[1], breed: row[2], id: row[0])
   end
 
   def self.find_by_name(name)
