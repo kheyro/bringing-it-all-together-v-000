89f89990345373656dbc7b63c2f889d447620551
Denis Atkesone <denis.atkesone@gmail.com>
2018-02-06 15:36:34 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -20,6 +20,20 @@ class Dog
     DB[:conn].execute(sql)
   end
 
+  def save
+    if !self.id
+      update
+    else
+      sql = <<-SQL
+        INSERT INTO dogs (name, breed)
+        VALUES (?, ?)
+      SQL
+
+      DB[:conn].execute(sql, self.name, self.breed)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+    end
+  end
+
   def self.drop_table
     DB[:conn].execute("DROP TABLE IF EXISTS dogs")
   end
